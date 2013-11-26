6e20f4d2aded8b4ec02222c772968ca80f0040a7
Stephen Hesperus Wong <StephenHesperusWong@gmail.com>
2013-11-26 16:23:23 +0800

    Implement LoaderCallbacks<Cursor>
    Use CursorLoader to do the book list query in the background and notify
    book list adapter when load finished.

--- EpubReader/src/com/blogspot/stewannahavefun/epubreader/EpubReaderActivity.java
+++ EpubReader/src/com/blogspot/stewannahavefun/epubreader/EpubReaderActivity.java
@@ -1,13 +1,19 @@
 package com.blogspot.stewannahavefun.epubreader;
 
 import android.app.Activity;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.GridView;
+import android.widget.SimpleCursorAdapter;
 
 import com.blogspot.stewannahavefun.epubreader.EpubReader.Books;
 
-public class EpubReaderActivity extends Activity {
+public class EpubReaderActivity extends Activity implements
+		LoaderCallbacks<Cursor> {
 	private SimpleCursorAdapter mAdapter;
 
 	@Override
@@ -40,6 +46,26 @@ public class EpubReaderActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.epub_reader, menu);
 		return true;
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		// TODO Auto-generated method stub
+		return new CursorLoader(
+				this,
+				Books.BOOKS_URI,
+				EpubReader.BOOK_LIST_PROJECTION,
+				null, null, null);
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		mAdapter.swapCursor(cursor);
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		mAdapter.swapCursor(null);
 	}
 
 }
