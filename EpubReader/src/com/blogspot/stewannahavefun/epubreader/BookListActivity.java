bac5937447ede39b8e4cc6bb6419e07e8dfc5f3c
Stephen Hesperus Wong <StephenHesperusWong@gmail.com>
2013-11-28 14:48:03 +0800

    Add book list OnItemClickListener
    This click listener starts ReadingActivity.

--- EpubReader/src/com/blogspot/stewannahavefun/epubreader/BookListActivity.java
+++ EpubReader/src/com/blogspot/stewannahavefun/epubreader/BookListActivity.java
@@ -2,11 +2,17 @@ package com.blogspot.stewannahavefun.epubreader;
 
 import android.app.Activity;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.content.ContentUris;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.SimpleCursorAdapter;
 
@@ -39,6 +45,33 @@ public class BookListActivity extends Activity implements
 
 		GridView bookList = (GridView) findViewById(R.id.book_list);
 		bookList.setAdapter(mAdapter);
+		bookList.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				onBookClick(id);
+			}
+		});
+	}
+
+	private void onBookClick(final long id) {
+		Uri bookIdUri = ContentUris.withAppendedId(Books.BOOK_ID_URI_BASE, id);
+		Cursor c = getContentResolver().query(
+				bookIdUri,
+				EpubReader.READ_BOOK_PROJECTION,
+				null, null, null);
+		
+		Bundle args = new Bundle();
+		args.putString(EpubReader.READ_BOOK_PROJECTION[1], c.getString(1));
+		args.putInt(EpubReader.READ_BOOK_PROJECTION[2], c.getInt(2));
+		args.putInt(EpubReader.READ_BOOK_PROJECTION[3], c.getInt(3));
+		args.putString(EpubReader.READ_BOOK_PROJECTION[4], c.getString(4));
+		
+		Intent reading = new Intent(this, ReadingActivity.class);
+		reading.putExtras(args);
+		
+		startActivity(reading);
 	}
 
 	@Override
