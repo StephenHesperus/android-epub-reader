f36951ea2e88ed0246abffc628bb5e5b0f2264a0
Stephen Hesperus Wong <StephenHesperusWong@gmail.com>
2013-12-07 00:46:27 +0800

    Add EpubFileProcessor class
    This class do the heavy work related to a potentially valid epub file.
    It checks if the file is truely an epub file, and unzip it if it is.
    It backs up the original file, update the database.

---
+++ EpubReader/src/com/blogspot/stewannahavefun/epubreader/EpubFileProcessor.java
@@ -0,0 +1,15 @@
+package com.blogspot.stewannahavefun.epubreader;
+
+import java.io.File;
+
+public class EpubFileProcessor {
+
+	private File mEpub;
+	private File mOutput;
+
+	public EpubFileProcessor(File epub, File output) {
+		mEpub = epub;
+		mOutput = output;
+	}
+
+}
