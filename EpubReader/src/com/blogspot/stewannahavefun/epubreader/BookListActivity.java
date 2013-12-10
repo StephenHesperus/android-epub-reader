828cabf39f97dbe5a278a5028062c136e6cce9f9
Stephen Hesperus Wong <StephenHesperusWong@gmail.com>
2013-12-11 00:20:40 +0800

    Update menu file resource reference

--- EpubReader/src/com/blogspot/stewannahavefun/epubreader/BookListActivity.java
+++ EpubReader/src/com/blogspot/stewannahavefun/epubreader/BookListActivity.java
@@ -112,7 +112,7 @@ public class BookListActivity extends Activity implements
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.epub_reader, menu);
+		getMenuInflater().inflate(R.menu.booklist, menu);
 		return true;
 	}
 
