f4029c446837d29170ab54b793f2dd905aec6899
Stephen Hesperus Wong <StephenHesperusWong@gmail.com>
2013-12-11 22:27:43 +0800

    Add ProcessorReceiver BroadcastReceiver
    This version of the receiver receives ACTION_DUPLICATION broadcast,
    which is sent by ProcessEpubFileService. When action matches, it makes a
    Toast saying the file exists.
    The receiver may receive other broadcast in future versions.

--- EpubReader/src/com/blogspot/stewannahavefun/epubreader/BookListActivity.java
+++ EpubReader/src/com/blogspot/stewannahavefun/epubreader/BookListActivity.java
@@ -4,9 +4,11 @@ import java.io.File;
 
 import android.app.Activity;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
@@ -20,16 +22,35 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.SimpleCursorAdapter.ViewBinder;
+import android.widget.Toast;
 
 import com.blogspot.stewannahavefun.epubreader.EpubReader.Books;
 
 public class BookListActivity extends Activity implements
 		LoaderCallbacks<Cursor>, FilePickerDialog.OnFilePickListener {
 
+	private class ProcessorReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if (ACTION_DUPLICATION.equals(intent.getAction())) {
+				String filename = intent
+						.getStringExtra(ACTION_DUPLICATION_EXTRA);
+				String msg = filename + " already exists!";
+
+				Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+			}
+		}
+
+	}
+
 	private static final String DIALOG_OPEN_EPUB_FILE = "DIALOG_OPEN_EPUB_FILE";
 	protected static final String SUFFIX_ADDED_DATE = "Added";
 	protected static final String SUFFIX_LAST_READING_DATE = "Last Read";
+	private static final String ACTION_DUPLICATION = "com.blogspot.stewannahavefun.epubreader.ACTION_DUPLICATION";
+	private static final String ACTION_DUPLICATION_EXTRA = "com.blogspot.stewannahavefun.epubreader.ACTION_DUPLICATION_EXTRA";
 	private SimpleCursorAdapter mAdapter;
+	private ProcessorReceiver mReceiver;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
