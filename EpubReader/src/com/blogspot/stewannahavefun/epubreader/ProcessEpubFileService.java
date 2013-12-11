01d3b6ab0febe48293802123f84a374a0c03c523
Stephen Hesperus Wong <StephenHesperusWong@gmail.com>
2013-12-11 10:29:05 +0800

    Use getData to get file info in process service
    Since the only needed argument is the file Uri. The base of the output
    directory will be a predefined variable, and the subdirectory is
    generated from the epub file name.

--- EpubReader/src/com/blogspot/stewannahavefun/epubreader/ProcessEpubFileService.java
+++ EpubReader/src/com/blogspot/stewannahavefun/epubreader/ProcessEpubFileService.java
@@ -2,14 +2,16 @@ package com.blogspot.stewannahavefun.epubreader;
 
 import java.io.File;
 
+import android.app.IntentService;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+
 import com.blogspot.stewannahavefun.epubreader.EpubFileProcessor.FileIsNotConstructedException;
 import com.blogspot.stewannahavefun.epubreader.EpubFileProcessor.UnsupportedFileException;
 import com.blogspot.stewannahavefun.epubreader.EpubReader.Books;
 import com.blogspot.stewannahavefun.epubreader.EpubReader.Contents;
-
-import android.app.IntentService;
-import android.content.ContentValues;
-import android.content.Intent;
 
 public class ProcessEpubFileService extends IntentService {
 	public class Processor extends EpubFileProcessor {
@@ -29,6 +31,8 @@ public class ProcessEpubFileService extends IntentService {
 	private static final String EXTRA_EPUB_PATH = "EXTRA_EPUB_PATH";
 	private static final String EXTRA_OUTPUT_DIRECTORY = "EXTRA_OUTPUT_DIRECTORY";
 	private static final String NCX_PATH = "NCX_PATH";
+	private final String mBase = Environment.getExternalStorageDirectory()
+			.getAbsolutePath() + "/epubreader-test/data/";
 
 	public ProcessEpubFileService() {
 		super(TAG);
@@ -36,23 +40,26 @@ public class ProcessEpubFileService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
-		String epubPath = intent.getStringExtra(EXTRA_EPUB_PATH);
-		String outputPath = intent.getStringExtra(EXTRA_OUTPUT_DIRECTORY);
-		File epub = new File(epubPath);
-		File output = new File(outputPath);
+		Uri data = intent.getData();
+		File epub = new File(data.getPath());
+		File base = new File(mBase);
+		File output = new File(base, epub.getName());
+
+		output.mkdir();
+
 		Processor processor = new Processor(epub, output);
-		
+
 		try {
 			boolean success = processor.unZipEpubFile();
-			
+
 			if (success) {
 				processor.backupEpubFile();
 				processor.readContainerDotXmlFile();
-				
+
 				ContentValues bookInfo = processor.readOpfFile();
 				bookInfo.remove(NCX_PATH);
 				getContentResolver().insert(Books.BOOKS_URI, bookInfo);
-				
+
 				processor.readNcxFile();
 			}
 		} catch (UnsupportedFileException e) {
