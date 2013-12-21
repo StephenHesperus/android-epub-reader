9a329da0748e03a47c53f693ecd7242b5b56c57b
Stephen Hesperus Wong <StephenHesperusWong@gmail.com>
2013-12-21 16:44:50 +0800

    Implement getJS_RegisterImageListener
    This method returns a string exactly the same as you will put into a
    <script> tag. The JavaScript content register a onImageClickListener on
    every <img> tag found on current page. The listener calls
    JavaScriptInterface method added to mBookView.

--- EpubReader/src/com/blogspot/stewannahavefun/epubreader/ReadingControl.java
+++ EpubReader/src/com/blogspot/stewannahavefun/epubreader/ReadingControl.java
@@ -1,5 +1,7 @@
 package com.blogspot.stewannahavefun.epubreader;
 
+import com.blogspot.stewannahavefun.epubreader.ReadingActivity.WebInterface;
+
 public class ReadingControl {
 
 	private static final String SCRIPT_ID = "-script-injected-by-epubreader";
@@ -10,5 +12,29 @@ public class ReadingControl {
 
 	public static String getImageListenerUrl() {
 		return getJSUrl(getJS_RegisterImageListener());
+	}
+
+	private static String getJS_RegisterImageListener() {
+		String registerJS = "var images = document.getElementsByTagName('img');"
+				+ "function onImageClickListener(e) {"
+				+ "var src = this.src;"
+				+ WebInterface.getInterfaceName()
+				+ "."
+				+ "onImageClick(src);"
+				+ "}"
+				+ "for (var i = 0; i < images.length; i++) {"
+				+ "images[i].addEventListener('click', onImageClickListener, false);"
+				+ "}";
+		String js = "var script = document.getElementById('" + SCRIPT_ID
+				+ "');"
+				+ "if (script == null) {"
+				+ "script = document.createElement('script');"
+				+ "script.id = '" + SCRIPT_ID + "';"
+				+ "script.setAttribute('type', 'text/javascript');"
+				+ "document.head.appendChild(script);"
+				+ "script.textContent = \"" + registerJS + "\";"
+				+ "}";
+
+		return js;
 	}
 }
